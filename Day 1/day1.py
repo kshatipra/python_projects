45f35bb1de22495696dfa671c6128f1323cda2a7
Pratiksha Pendse <pratiksha.pendse@rohde-schwarz.com>
2024-09-18 16:35:14 +0200

    day1 of drafting my learnings

---
+++ Day 1/day1.py
@@ -0,0 +1,38 @@
+# Day 1 of drafting code that I have learnt
+
+# 1. To display some information on the screen
+
+print("Hello world!") #printing a message
+print(":)") #printing an emoji
+print("12345") #printing numbers
+print("Hello This is a thirty day challenge\nI don't know why I am typing so much hahahhahahah but here it is\nto make this longggg!!") #printing a blockk, \n is to tell that we need a new line here after this. 
+
+# 2. To perform basic arithmetic operations
+
+print(5 + 3) #addition
+print(5 - 3) #subtraction
+print(5 * 3) #multiplication
+print(5 / 3) #division
+
+# 3. To assign values to variables
+
+x = 5 #assigning 5 to variable x
+y = 3 #assigning 3 to variable y
+
+print(x + y) #printing the sum of x and y
+print(x - y) #printing the sum of x and y
+print(x * y) #printing the sum of x and y
+print(x / y) #printing the sum of x and y
+print(x % y) #what do you think this is?
+
+#4. Example, Number of minutes in  two years
+num_of_minutes = 356*2*24*60 
+print(num_of_minutes)
+
+#So this was it, printing something.. 
+#What is happening?? :
+#The computer doesn’t understand what you are writing because it doesn’t speak our language. 
+# So, we use a common tongue called programming. We write code that the compiler understands. 
+# The compiler translates this code into a form that the computer can understand and execute. Finally, the computer processes this and displays the output.
+
+#What am I?? the sentances with # are comments, theser are for us and others to know whats happening, compiler just ignores them, just like our brain ignores important stuff !
