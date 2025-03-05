e126fa4799c88727ece83c48ed41e335972b05fa
kshatipra <pratikshapendse10@gmail.com>
2025-03-05 19:32:53 +0100

    completed fact_generator

--- Projects/fact_generator.py
+++ Projects/fact_generator.py
@@ -1,22 +1,20 @@
 # Write code here
 import requests, json
 
-
 def get_fact(number):
     if number > 7:
         print("Maximum number of facts per execution is 7")
-    elif number < 0:
+    elif number <= 0:
         print("Minimum number of facts per execution is 1")
+    else:
+        while number != 0:
+            response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+            number = number - 1
+            content = response.text
+            data = json.loads(content)
+            fact = data['text']
+            print(fact)
 
-    
-    while number != 0:
-        response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
-        number = number - 1
-        content = response.text
-        data = json.loads(content)
-        fact = data['text']
-        print(fact)
-
-user_input = input("Enter a number")
+user_input = input()
 num = int(user_input)
 facts = get_fact(num)
