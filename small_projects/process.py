09ad7fd5c068839cc62d325f7c4a9661d0394826
Pratiksha Pendse <pratiksha.pendse@rohde-schwarz.com>
2025-05-22 10:46:27 +0200

    CSES Algorithms wierd algorithm and process basics

---
+++ small_projects/process.py
@@ -0,0 +1,11 @@
+# import os, psutil
+
+# process = psutil.Process(os.getpid) #creates Process object and assigns it to variable process with the processid of the your currenzt python script
+# print("Process ID:", process.pid)
+# print("Process name:", process.name())
+# print("Memory usage:", process.memory_info())
+# print("Memory usage in Bytes:", process.memory_info().rss/(1024**2))
+# print("CPU Usage:", process.cpu_percent)
+# print("Status:", process.status())
+
+# print()
